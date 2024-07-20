Fixed AUC calculation to use correct input and target. Updated logging system.

classifiers/hybrid/learn.py
@@ -9,7 +9,7 @@ from classifiers.utils import train, test
 from models import *
 from data.event_dataset import EventDataset
 
-blur_size = 0.05
+blur_size = 0.10
 feature_cols = [
 	"px_0", "py_0", "pz_0", "energy_0",
 	"px_1", "py_1", "pz_1", "energy_1",
@@ -31,17 +31,17 @@ train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
 model = LatentSpacePooledHybridClassifier(16,
-                                          [128, 128, 128],
-                                          [128, 128, 128],
+                                          [128, 128],
+                                          [128, 128],
                                           [64, 64, 64, 64, 64],
-                                          [512, 256, 256, 128])
+                                          [512, 256, 128])
 
-lr = 0.001
+lr = 0.0001
 weight_decay = 0.01
 loss_function = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr, weight_decay=weight_decay)
 
-epochs = 10
+epochs = 20
 loss_over_time = []
 accuracy_over_time = []
 auc = []

classifiers/invariant/learn.py
@@ -9,7 +9,7 @@ from classifiers.utils import train, test
 from models import LorenzInvariantNetwork
 from data.event_dataset import EventDataset
 
-blur_size = 0.05
+blur_size = 0.10
 feature_cols = [
 	"px_0", "py_0", "pz_0", "energy_0",
 	"px_1", "py_1", "pz_1", "energy_1",
@@ -37,7 +37,7 @@ weight_decay = 0.001
 loss_function = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.AdamW(model.parameters(), lr, weight_decay=weight_decay)
 
-epochs = 10
+epochs = 30
 loss_over_time = []
 accuracy_over_time = []
 auc = []
@@ -47,7 +47,7 @@ metric = BinaryAUROC()
 for t in range(epochs):
 	print(f"Epoch {t + 1}\n-------------------------------")
 	train(train_dataloader, model, loss_function, optimizer, True)
-	loss, acc, auc_metric = test(test_dataloader, model, loss_function, True)
+	loss, acc, auc_metric = test(test_dataloader, model, loss_function, metric, True)
 	
 	loss_over_time.append(loss)
 	accuracy_over_time.append(acc)
@@ -68,11 +68,11 @@ plt.title("Lorentz-invariant Classifier Accuracy per Epoch")
 plt.show()
 
 log = pl.DataFrame({
-	"epoch": list(range(1, max_acc_epoch + 1)),
-	"loss": loss_over_time[:max_acc_epoch],
-	"acc": accuracy_over_time[:max_acc_epoch],
+	"epoch": list(range(1, epochs + 1)),
+	"loss": loss_over_time,
+	"acc": accuracy_over_time,
 	"auc": auc,
-	"model": "General PFN Classifier",
+	"model": "Lorenz-invariant Classifier",
 	"lr": lr,
 	"weight_decay": weight_decay,
 	"blur_size": blur_size

classifiers/utils.py
@@ -52,8 +52,8 @@ def test(dataloader, model, loss_fn, metric, print_results=False) -> tuple[float
 			pred = model(X)
 			test_loss += loss_fn(pred, y).item()
 			
-			auc_input = torch.cat((auc_input, y.reshape((-1))))
-			auc_target = torch.cat((auc_target, torch.nn.functional.sigmoid(pred).reshape((-1))))
+			auc_input = torch.cat((auc_input, torch.nn.functional.sigmoid(pred).reshape((-1))))
+			auc_target = torch.cat((auc_target, y.reshape((-1))))
 			
 			for i_y, i_pred in zip(list(y), list(pred)):
 				i_y = i_y.numpy()
@@ -67,7 +67,7 @@ def test(dataloader, model, loss_fn, metric, print_results=False) -> tuple[float
 	
 	if print_results:
 		print(f"Test Error: Avg loss: {test_loss:>8f}")
-		print(f"Accuracy: {correct}/{size:>0.1f} = {correct / size * 100:<0.2f}% \n")
-		print(f"AUC: {auc:>0.1f} \n")
+		print(f"Accuracy: {correct}/{size:>0.1f} = {correct / size * 100:<0.2f}%")
+		print(f"AUC: {auc:>0.3f} \n")
 	
 	return test_loss, correct / size, auc